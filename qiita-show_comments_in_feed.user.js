07dfc282d96006f81ed4905d5b0e0d39c8319c88
vzvu3k6k <vzvu3k6k@gmail.com>
2014-12-07 14:12:28 +0900

    Just apply target='_blank' (close #3)
    When submitting, editing or deleting a comment, the post page will be
    opened in a new window. And comments in the feed page is not changed.
    
    This is not so good experience, but it makes maintenance easier.
    
    Note:
    If a "Submit" button in a comment form is clicked, eventually
    jQuery(form).submit() will be called. The entire process is handled by
    Qiita's JavaScript.
    
    If a link to delete a comment is clicked, the event is caught by
    "Ruby on Rails unobtrusive scripting adapter for jQuery"
    (https://github.com/rails/jquery-ujs), and it sends a request to the server.

--- qiita-show_comments_in_feed.user.js
+++ qiita-show_comments_in_feed.user.js
@@ -43,22 +43,12 @@ location.href = 'javascript:void(' + function(){
   function insertWriteCommentButton($itemBox){
     var $button = document.createElement('a');
     $button.setAttribute('class', 'btn btn-primary');
+    $button.setAttribute('href', 'javascript:void(0)');
     $button.textContent = I18n.lookup('js.item_box.comment') || 'Comment';
-
-    if(enableNewCommentForm) {
-      $button.setAttribute('href', 'javascript:void(0)');
-      $button.addEventListener('click', function(event){
-        $button.remove();
-        insertComment($itemBox);
-      });
-    }else{
-      $button.setAttribute(
-        'href',
-        $itemBox.querySelector('.item-box-title a').href + '#new_comment'
-      );
-      $button.setAttribute('target', '_blank');
-    }
-
+    $button.addEventListener('click', function(event){
+      $button.remove();
+      insertComment($itemBox);
+    });
     $itemBox.querySelector('.item-body-wrapper').appendChild($button);
   }
 
@@ -102,61 +92,28 @@ location.href = 'javascript:void(' + function(){
         });
       }catch(e){ }
 
-      /* Enable/Disable the new comment form */
+      /* Enable the new comment form */
       var $$newComment = $comments.querySelector('.js-new-comment');
       try{
-        if(!enableNewCommentForm) throw 'new comment form is disabled';
-
-        var newCommentView = new Qiita.views.items.NewCommentView({
+        new Qiita.views.items.NewCommentView({
           el: $$newComment,
           collection: item.comments,
           enableAsyncPost: false
         });
-
-        /*
-         Absolutify action URL.
-         As `action` is either `/comments` or `/comments/{id}`
-         for now, this is not necessary.
-         Added as a precaution.
-         */
-        var actionUrl = (function(){
-          var a = responseDocument.createElement('a');
-          a.setAttribute('href', newCommentView.formView.$el.attr('action'));
-          return a.href;
-        })();
-
-        newCommentView.formView.$el.submit = function(){
-          jQuery.post(actionUrl, newCommentView.formView.$el.serialize(), null, 'html')
-            .then(null, function(){
-              Qiita.notification.error('[UserScript: Qiita: Show comments in feed] Fail to submit.');
-              return jQuery.Deferred(); /* Cancel following chains */
-            }).done(function(){
-              /* Rebuild comments */
-              $comments.remove();
-              insertComment($itemBox);
-            }).fail(function(){
-              Qiita.notification.error('[UserScript: Qiita: Show comments in feed] Fail to render comments.');
-            });
-        };
       }catch(e){
         $$newComment.style.display = 'none';
       }
+
+      Array.prototype.forEach.call(
+        $comments.querySelectorAll(
+          '.commentHeader_deleteButton a, form'
+        ),
+        function(i){
+          i.setAttribute('target', '_blank');
+        }
+      );
     };
     xhr.responseType = 'document';
     xhr.send();
   }
-
-  /* Check roughly if relevant codes are not changed */
-  var enableNewCommentForm = false;
-  try{
-    enableNewCommentForm = [
-      ['Qiita.views.items.NewCommentView.prototype.initialize', 153],
-      ['Qiita.views.items.NewCommentView.prototype.resetFormView', 232],
-      ['Qiita.views.items.CommentFormView.prototype.onClickSubmit', 53],
-      ['Qiita.views.items.CommentFormView.prototype.submit', 177]
-    ].every(function(i){
-      return eval(i[0]).toString().length === i[1];
-    });
-  }catch(e){ }
-
 } + ')()';
